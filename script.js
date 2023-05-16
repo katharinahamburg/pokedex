a6c26ce7a78403cc87a36ec92989f19e4dd29605
katharinahamburg <katharina@hinnrichs.de>
2023-05-16 14:52:35 +0200

    fetch API and rendering

---
+++ script.js
@@ -0,0 +1,14 @@
+async function loadPokemon(){
+    let url =`https://pokeapi.co/api/v2/pokemon/ivysaur`
+    let response = await fetch(url);
+    currentPokemon =  await response.json();
+    console.log ('loaded pokemon', currentPokemon);
+
+    renderPokemonInfo();
+}
+
+function renderPokemonInfo(){
+document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+}
+
+
